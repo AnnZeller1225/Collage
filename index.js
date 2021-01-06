70601d8519ac29c21d7b04baf5c725309a8132c0
AnnZeller1225 <annazeller@mail.com>
2021-01-06 17:40:47 +0300

    added drag some pictures

--- index.js
+++ index.js
@@ -1,66 +1,145 @@
 let cat = document.querySelector(".cat");
 let sofa = document.querySelector(".sofa");
+let palm = document.querySelector(".palm");
+let images = document.querySelectorAll(".box");
+let field = document.querySelector(".field");
+
 let currentDroppable = null;
+
+const highlight = (el) => {
+  el.classList.toggle("highlight");
+};
+const deleteHighlight = (el) => {
+  el.classList.remove("highlight");
+}
 
 const move = (el) => {
   el.onmousedown = function (event) {
-    let shiftX = event.clientX - el.getBoundingClientRect().left;
-    let shiftY = event.clientY - el.getBoundingClientRect().top;
+    let target = event.currentTarget;
 
-    el.style.position = "absolute";
-    el.style.zIndex = 1000;
-    document.body.append(el);
+    // console.log(target.className)
+    if (target.className === "selectWrap") {
+      console.log(" its wrap");
+      let children = target.querySelectorAll(".box");
 
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-      el.style.left = pageX - shiftX + "px";
-      el.style.top = pageY - shiftY + "px";
+      children.forEach((el) => {  
+        // let elem =
+        deleteHighlight(el);
+        document.body.append(el);
+      });
+      target.classList.toggle('hide');
     }
 
-    function onMouseMove(event) {
+    if (!event.ctrlKey) {
+      // console.log(' pressed without ctrl ', el)
+      let shiftX = event.clientX - el.getBoundingClientRect().left;
+      let shiftY = event.clientY - el.getBoundingClientRect().top;
+
+      el.style.position = "absolute";
+      highlight(el);
+      el.style.zIndex = 1; // для наслаивания
+      document.body.append(el);
       moveAt(event.pageX, event.pageY);
 
-      el.hidden = true;
-      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-      el.hidden = false;
+      function moveAt(pageX, pageY) {
+        el.style.left = pageX - shiftX + "px";
+        el.style.top = pageY - shiftY + "px";
+      }
 
-      if (!elemBelow) return;
+      function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
 
-      let droppableBelow = elemBelow.closest(".droppable");
-      if (currentDroppable != droppableBelow) {
-        if (currentDroppable) {
-          // null если мы были не над droppable до этого события
-          // (например, над пустым пространством)
-          leaveDroppable(currentDroppable);
-        }
-        currentDroppable = droppableBelow;
-        if (currentDroppable) {
-          // null если мы не над droppable сейчас, во время этого события
-          // (например, только что покинули droppable)
-          enterDroppable(currentDroppable);
+        el.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        el.hidden = false;
+
+        if (!elemBelow) return;
+
+        let droppableBelow = elemBelow.closest(".droppable");
+        if (currentDroppable != droppableBelow) {
+          currentDroppable = droppableBelow;
         }
       }
-    }
 
-    document.addEventListener("mousemove", onMouseMove);
+      document.addEventListener("mousemove", onMouseMove);
 
-    el.onmouseup = function () {
-      document.removeEventListener("mousemove", onMouseMove);
-      el.onmouseup = null;
-    };
-
-    function enterDroppable(elem) {
-      elem.style.background = "pink";
-    }
-
-    function leaveDroppable(elem) {
-      elem.style.background = "";
+      el.onmouseup = function () {
+        document.removeEventListener("mousemove", onMouseMove);
+        el.onmouseup = null;
+        highlight(el);
+      };
+      el.ondragstart = function () {
+        return false;
+      };
     }
   };
   el.ondragstart = function () {
     return false;
   };
 };
+let selectedGroupWrap = document.querySelector(".selectWrap");
+// selectedGroupWrap.classList.add("selectWrap");
+// document.body.append(selectedGroupWrap);
+
+let selectedGroup = [];
+
+const selectSome = (event) => {
+  console.log("selectSome start");
+
+  if (event.ctrlKey) {
+    let target = event.currentTarget;
+    selectedGroup.push(target.id);
+
+    selectedGroupWrap.append(target);
+    highlight(target);
+  }
+};
+
 move(sofa);
 move(cat);
+move(palm);
+move(selectedGroupWrap);
+
+images.forEach((el) => {
+  el.addEventListener("mousedown", selectSome);
+  // el.addEventListener("mouseup", selectSome);
+});
+const resize = () => {
+  var p = document.querySelector("p");
+  p.addEventListener(
+    "click",
+    function init() {
+      p.removeEventListener("click", init, false);
+      p.className = p.className + " resizable";
+      var resizer = document.createElement("div");
+      resizer.className = "resizer";
+      p.appendChild(resizer);
+      resizer.addEventListener("mousedown", initDrag, false);
+    },
+    false
+  );
+
+  var startX, startY, startWidth, startHeight;
+
+  function initDrag(e) {
+    startX = e.clientX;
+    startY = e.clientY;
+    startWidth = parseInt(document.defaultView.getComputedStyle(p).width, 10);
+    startHeight = parseInt(document.defaultView.getComputedStyle(p).height, 10);
+    document.documentElement.addEventListener("mousemove", doDrag, false);
+    document.documentElement.addEventListener("mouseup", stopDrag, false);
+  }
+
+  function doDrag(e) {
+    p.style.width = startWidth + e.clientX - startX + "px";
+    p.style.height = startHeight + e.clientY - startY + "px";
+  }
+
+  function stopDrag(e) {
+    document.documentElement.removeEventListener("mousemove", doDrag, false);
+    document.documentElement.removeEventListener("mouseup", stopDrag, false);
+  }
+};
+resize();
+
+
